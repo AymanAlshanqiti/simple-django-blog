97cfa67c5e98ff7bceb5ea65a48cb63aca190b22
Ayman <ayman.alshanqiti@gmail.com>
2021-06-02 11:40:27 +0300

    feat: add Article model to articles app

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    source_id = models.CharField(max_length=64, blank=True)
+    source_name = models.CharField(max_length=64)
+    author = models.CharField(max_length=64)
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    url = models.CharField(max_length=128)
+    url_to_image = models.CharField(max_length=128)
+    published_at = models.DateTimeField(auto_now_add=False)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
