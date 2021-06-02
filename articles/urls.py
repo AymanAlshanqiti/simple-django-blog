1996918f9c4d75f50e06212d7e00dda37c2e6398
Ayman <ayman.alshanqiti@gmail.com>
2021-06-02 15:36:18 +0300

    feat: finish the base of article detail

--- articles/urls.py
+++ articles/urls.py
@@ -3,4 +3,5 @@ from articles import views
 
 urlpatterns = [
     path('articles/', views.articles_list, name='articles_list'),
+    path('articles/<int:article_id>', views.article_detail, name='article_detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from articles.models import Article
+from django.http import HttpResponse
+
 # Create your views here.
 
 
@@ -7,3 +10,9 @@ def articles_list(request):
     articles = Article.objects.all()
     context = {"articles": articles}
     return render(request, 'articles_list.html', context)
+
+
+def article_detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    context = {"article": article}
+    return render(request, 'article_detail.html', context)
