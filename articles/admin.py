642f9aa98bfd2bd4dcc4467a4150429c346429e4
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2021-06-06 00:34:21 +0300

    feat: add the ability to user to add/remove articles to his favourite list (#1)

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article
+from articles.models import Article, FavouriteArticle
 
-# admin.site.register(FavouriteArticle)
 admin.site.register(Article)
+admin.site.register(FavouriteArticle)
