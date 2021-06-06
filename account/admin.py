086ed3d51ffc14ac44f34d6fc9f31859aceb2c22
Ayman <ayman.alshanqiti@gmail.com>
2021-06-06 06:32:05 +0300

    feat: restyle admin tables

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from account.models import Account
 
-admin.site.register(Account)
+
+class AccountAdmin(UserAdmin):
+    list_display = ['email', 'name', 'phone_number', 'date_of_birth',
+                    'last_login', 'is_active', 'is_admin', 'is_staff']
+    search_fields = ['email', 'name', 'phone_number', 'national_id']
+    readonly_fields = ['date_joined', 'last_login']
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
